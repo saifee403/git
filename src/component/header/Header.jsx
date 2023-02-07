d3fc419842ef41607df02dc699025cab796c22c2
saifee403 <muh.saif2001@gmail.com>
2023-02-07 15:22:50 +0500

    change color of navbar

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,11 +1,14 @@
 import "./header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
     <div className="header">
       <div className="headerList">
         <div className="headerListItem">
-            
+        <FontAwesomeIcon icon={faBed} />
+        <span>Stay</span>
         </div>
       </div>
     </div>
